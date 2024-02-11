555f00c76f3cb02cc73ea9bdcc75258e633bd93f
Julio Jimenez <julius.1986@gmail.com>
2024-02-11 12:53:20 +0000

    Primer commit

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, MinLength, IsInt, IsPositive, Min } from "class-validator";
+
+
+export class CreatePokemonDto {
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number
+
+    @IsString()
+    @MinLength(1)
+    name: string
+}
